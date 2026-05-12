8cb882af4ff351df24c8e82f9f1a189d7a09b85a
Balaji K <balajik@Balajis-MacBook-Air.local>
2026-05-12 17:14:20 +0530

    fix: added missing Python controllers for new DocTypes

---
+++ hospitality_erp/cinema/doctype/cinema_hall/cinema_hall.py
@@ -0,0 +1,5 @@
+import frappe
+from frappe.model.document import Document
+
+class CinemaHall(Document):
+	pass
---
+++ hospitality_erp/theme_park/doctype/theme_park_attraction/theme_park_attraction.py
@@ -0,0 +1,5 @@
+import frappe
+from frappe.model.document import Document
+
+class ThemeParkAttraction(Document):
+	pass
