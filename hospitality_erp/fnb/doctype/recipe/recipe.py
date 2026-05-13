b98e1a85068a0a7ae6080484fac1440e81a6e30c
Balaji K <balajik@Balajis-MacBook-Air.local>
2026-05-13 12:34:35 +0530

    feat: added boilerplate python controllers for new DocTypes to fix migration errors

--- hospitality_erp/fnb/doctype/recipe/recipe.py
+++ hospitality_erp/fnb/doctype/recipe/recipe.py
@@ -1,6 +1,5 @@
+import frappe
 from frappe.model.document import Document
+
 class Recipe(Document):
-    def validate(self):
-        for r in (self.ingredients or []):
-            r.total_cost = (r.quantity or 0) * (r.cost_per_unit or 0)
-        self.total_cost = sum(r.total_cost or 0 for r in (self.ingredients or []))
+	pass
