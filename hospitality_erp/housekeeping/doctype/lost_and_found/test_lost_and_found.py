2299fb98645a52eb4dbe72e9117440b4e8615aeb
Balaji K <balajik@Balajis-MacBook-Air.local>
2026-05-13 15:28:28 +0530

    chore: remove Lost and Found DocType as requested

--- hospitality_erp/housekeeping/doctype/lost_and_found/test_lost_and_found.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, Your Company and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestLostandFound(FrappeTestCase):
-	pass
